3c960e32edc9363edc1c202e76d3ca07c46a2eba
Nathan Adler <nathanadler1710@gmail.com>
2017-07-23 23:29:23 +1000

    Working sentiment analysis method added.

--- my_test.py
+++ my_test.py
@@ -1,8 +1,24 @@
 import sys
 from time import sleep
 
+import RPi.GPIO as GPIO
+
 import twitter
 from credentials import *
+
+import re
+from textblob import TextBlob
+
+def clean_tweet(tweet):
+	'''
+	Utility function to clean tweet text by removing links, special characters
+	using simple regex statements.
+	'''
+	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analyse_tweet(tweet):
+	analysis = TextBlob(clean_tweet(tweet))
+	return analysis.sentiment.polarity
 
 api = twitter.Api(consumer_key=CONSUMER_KEY,
                 consumer_secret=CONSUMER_SECRET,
@@ -17,5 +33,7 @@ api = twitter.Api(consumer_key=CONSUMER_KEY,
 stream = api.GetStreamFilter(track=['curiosity'])
 
 for line in stream:
-	print line['text']
+	text = line['text']
+	print text
+	print 'Sentiment: ' + str(analyse_tweet(text))
 	print '###########'
--- setup_pi.sh
+++ setup_pi.sh
@@ -2,7 +2,10 @@
 
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install python-dev python-pip nodejs build-essential
-sudo apt-get install screen
+sudo apt-get install vim screen
+
+echo "syntax on" >> ~/.vimrc
+echo "tabstop=4 shiftwidth=4" >> ~/.vimrc
 
 sudo pip install --upgrade pip
 sudo pip install virtualenv virtualenvwrapper
@@ -11,14 +14,17 @@ mkdir ~/.virtualenvs
 export WORKON_HOME=~/.virtualenvs
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 source ~/.bashrc
+
 mkvirtualenv bear
+pip install python-twitter textblob RPi.GPIO
+python -m textblob.download_corpora
 deactivate
 
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-sudo ln -sf /usr/local/n/versions/node/*/bin/node /usr/bin/nodejs
-npm install twitter
+#sudo npm cache clean -f
+#sudo npm install -g n
+#sudo n stable
+#sudo ln -sf /usr/local/n/versions/node/*/bin/node /usr/bin/nodejs
+#npm install twitter
 
 cd ~/Downloads
 wget -O Hamachi.tgz https://www.vpn.net/installers/logmein-hamachi-2.1.0.174-armhf.tgz
