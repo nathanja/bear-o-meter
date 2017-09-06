8aefaa25b98c85b73bad4bb4167dfc4a850900d8
Nathan Adler <nathanadler1710@gmail.com>
2017-09-06 22:01:34 +1000

    Adding proper gpio control; modularising functions.

--- my_test.py
+++ my_test.py
@@ -10,19 +10,44 @@ from credentials import *
 import re
 from textblob import TextBlob
 
-channel = 11
+# Global constants/variables
+channels = [3,5]
+fan_balance = 0
+gpio_initialised = False
 
-bear_id = 888598391298465792
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(channel, GPIO.OUT, initial=GPIO.LOW)
+def init_gpio():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(channels, GPIO.OUT, initial=GPIO.LOW)
+	gpio_initialised = True
 
 #GPIO.output(channel, GPIO.HIGH)
 
 def signal_handler(signal, frame):
 	print 'Caught SIGINT - exiting cleanly...'
-	GPIO.cleanup()
+	if gpio_initialised:
+		GPIO.cleanup()
+		gpio_initialised = False
+
 	sys.exit()
+
+
+def inflate(power='FULL'):
+	if power == 'FULL':
+		GPIO.output(channels, GPIO.HIGH)
+	elif power == 'OFF':
+		GPIO.output(channels, GPIO.LOW)
+	elif power == 'HALF':
+		if fan_balance == 0:
+			GPIO.output(channels, (GPIO.HIGH, GPIO.LOW))
+			fan_balance = 1
+		else:
+			GPIO.output(channels, (GPIO.LOW, GPIO.HIGH))
+			fan_balance = 0
+	else:
+		print('Warning: unknown power level input!')
+		GPIO.output(channels, GPIO.LOW)	
+
 
 def clean_tweet(tweet):
 	'''
@@ -31,36 +56,55 @@ def clean_tweet(tweet):
 	'''
 	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
+
 def analyse_tweet(tweet):
-	analysis = TextBlob(clean_tweet(tweet))
+	#analysis = TextBlob(clean_tweet(tweet))
+	analysis = TextBlob(tweet)
 	return analysis.sentiment.polarity
 
-#signal.signal(signal.SIGINT, signal_handler)
 
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                consumer_secret=CONSUMER_SECRET,
-                access_token_key=ACCESS_TOKEN,
-                access_token_secret=ACCESS_SECRET)
+def analyse_stream(bear_id, track_list):
+	if not track_list:
+		print 'You gave me nothing to track!'
+		return
 
-#timeline = api.GetHomeTimeline()
-#print timeline
+	try:
+		stream = api.GetStreamFilter(track=track_list)
 
-#status = api.PostUpdate('Another post!')
+		for line in stream:
+			text = line['text']
+			sen_val = analyse_tweet(text)
+			print text
+			print 'Sentiment: ' + str(sen_val)
+			print '###########'
 
-try:
-	stream = api.GetStreamFilter(track=['#colabsau'])
+			if sen_val > 0.5 and line['user']['id'] != bear_id:
+				print 'Reposting!'
+				api.PostRetweet(line['id'])
 
-	for line in stream:
-		text = line['text']
-		sen_val = analyse_tweet(text)
-		print text
-		print 'Sentiment: ' + str(sen_val)
-		print '###########'
+	except:
+		print 'Error in reading stream!'
+		if gpio_initialised:
+			GPIO.cleanup()
+			gpio_initialised = False
 
-		if sen_val > 0.5 and line['user']['id'] != bear_id:
-			print 'Reposting!'
-			api.PostRetweet(line['id'])
 
-except:
-	print 'Error in reading stream!'
-	GPIO.cleanup()
+if __name__ == '__main__':
+
+	signal.signal(signal.SIGINT, signal_handler)
+
+	api = twitter.Api(consumer_key=CONSUMER_KEY,
+					consumer_secret=CONSUMER_SECRET,
+					access_token_key=ACCESS_TOKEN,
+					access_token_secret=ACCESS_SECRET)
+
+	#timeline = api.GetHomeTimeline()
+	#print timeline
+
+	#status = api.PostUpdate('Another post!')
+
+	myself = api.VerifyCredentials().AsDict()
+	print 'id: ', myself['id']
+	print 'name: ', myself['name']
+	print 'screen_name: ', myself['screen_name']
+	#analyse_stream(myself['id'], ['#colabsau','@'+myself['screen_name']
